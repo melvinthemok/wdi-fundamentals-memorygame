d6c0516d6c293df24110900638358d7ee29e994d
Mok <melvinmok@OKCOMPUTER.local>
2016-12-04 23:39:37 +0800

    Create cards with JavaScript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,10 +4,22 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-
+/*
 if (cardTwo === cardFour) {
   alert('You found a match!');
 }
 else {
   alert('Sorry, try again.');
 }
+*/
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function () {
+  for (var i = 0; i < 4; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    document.getElementById('game-board').appendChild(newCard); 
+  }
+}
+
+createCards();
